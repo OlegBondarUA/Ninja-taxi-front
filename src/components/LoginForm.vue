15e78ccda3715e568e02f7d7999c43bd722b42c9
OlegBondarUA <Petrovichteh@gmail.com>
2024-05-31 14:04:34 +0300

    upd top navbar, add login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="login-form">
+    <h2>Особистий кабінет</h2>
+    <form @submit.prevent="handleSubmit">
+      <div class="form-group">
+        <label for="login">Логін</label>
+        <input type="text" id="login" v-model="login" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Пароль</label>
+        <div class="password-input">
+          <input :type="showPassword ? 'text' : 'password'" id="password" v-model="password" required>
+          <button type="button" @click="toggleShowPassword">
+            <span v-if="showPassword" class="password-icon">&#x2713;</span>
+            <span v-else class="password-icon"></span>
+            Показати пароль
+          </button>
+        </div>
+      </div>
+      <div class="btn-group">
+        <div class="form-group-btn">
+          <router-link to="/forgot-password">Я забув пароль</router-link>
+        </div>
+        <button type="submit">Вхід</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      login: "",
+      password: "",
+      showPassword: false
+    };
+  },
+  methods: {
+    handleSubmit() {
+      // Логіка для входу
+      console.log("Login:", this.login);
+      console.log("Password:", this.password);
+    },
+    toggleShowPassword() {
+      this.showPassword = !this.showPassword;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.form-group {
+  margin-bottom: 15px;
+  width: 100%;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 10px 15px;
+  border: none;
+  background-color: #ec6323;
+  color: white;
+  border-radius: 20px;
+  cursor: pointer;
+}
+
+button[type="button"] {
+  background-color: transparent;
+  color: #ec6323;
+  margin-top: 5px;
+  margin-left: 15px;
+}
+
+button[type="button"]:hover {
+  text-decoration: underline;
+}
+
+.password-input {
+  position: relative;
+}
+
+.password-icon {
+  position: absolute;
+  top: 77%;
+  left: 0;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.password-icon::before {
+  content: '';
+}
+
+.btn-group {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.btn-group button {
+  width: 100%;
+}
+
+.btn-group a {
+  color: #ec6323;
+  text-decoration: none;
+  text-align: center;
+  border: 3px solid #ec6323;
+  border-radius: 20px;
+  padding: 5px 15px;
+}
+
+.form-group-btn {
+    width: 100%;
+}
+</style>
--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -2,14 +2,16 @@
   <nav class="header fixed-top" ref="headRef">
     <div class="container">
       <div class="logo">
-        <img src="/images-cloud/logo.svg" alt="logo">
+        <router-link to="/">
+          <img src="/images-cloud/logo.svg" alt="logo">
+        </router-link>
       </div>
       <ul class="nav me-auto">
         <li class="nav-item"><router-link to="/" class="nav-link px-2 active" aria-current="page">Головна</router-link>
         </li>
         <li class="nav-item dropdown" @mouseenter="showServiceDropdown = true"
           @mouseleave="showServiceDropdown = false">
-          <a href="#" class="nav-link px-2" id="navbarDropdown" role="button" aria-expanded="false">Обрати послугу</a>
+          <span class="nav-link px-2" id="navbarDropdown" role="button" aria-expanded="false">Обрати послугу</span>
           <ul class="dropdown-menu dropdown-menu-service" aria-labelledby="navbarDropdown"
             :style="{ display: showServiceDropdown ? 'block' : 'none' }" @mouseenter="showServiceDropdown = true"
             @mouseleave="showServiceDropdown = false">
@@ -20,7 +22,7 @@
         </li>
         <li class="nav-item dropdown" @mouseenter="showBusinessDropdown = true"
           @mouseleave="showBusinessDropdown = false">
-          <a href="#" class="nav-link px-2" id="navbarDropdownBusiness" role="button" aria-expanded="false">Для бізнесу</a>
+          <span class="nav-link px-2" id="navbarDropdownBusiness" role="button" aria-expanded="false">Для бізнесу</span>
           <ul class="dropdown-menu dropdown-menu-business" aria-labelledby="navbarDropdownBusiness"
             :style="{ display: showBusinessDropdown ? 'block' : 'none' }" @mouseenter="showBusinessDropdown = true"
             @mouseleave="showBusinessDropdown = false">
@@ -29,7 +31,7 @@
           </ul>
         </li>
         <li class="nav-item dropdown" @mouseenter="showIconsDropdown = true" @mouseleave="showIconsDropdown = false">
-          <a href="#" class="nav-link px-2" id="navbarDropdownIcons" role="button" aria-expanded="false">Приєднатися</a>
+          <span class="nav-link px-2" id="navbarDropdownIcons" role="button" aria-expanded="false">Приєднатися</span>
           <ul class="dropdown-menu social-icons" aria-labelledby="navbarDropdownIcons"
             :style="{ display: showIconsDropdown ? 'block' : 'none' }" @mouseenter="showIconsDropdown = true"
             @mouseleave="showIconsDropdown = false">
@@ -43,54 +45,87 @@
           </ul>
         </li>
       </ul>
-      <div class="sing-in dropdown">
-        <a href="#" class="d-block text-decoration-none" id="dropdownUser2" data-bs-toggle="dropdown"
-          aria-expanded="false" style="margin-top:5px">
-          <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M32.9948 17.0047C32.9948 22.106 30.6076 26.6578 26.8752 29.5802C24.1611 31.7259 20.7224 33 16.9995 33C13.2671 33 9.82841 31.7259 7.11437 29.5802C3.38672 26.6531 0.999512 22.106 0.999512 17.0047C0.999512 8.16163 8.16115 1 17.0042 1C25.8379 1 32.9948 8.16163 32.9948 17.0047Z"
-              fill="#A1E8B9" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-            <path
-              d="M23.6783 14.4942C23.6783 10.8045 20.6896 7.8157 16.9998 7.8157C13.31 7.8157 10.3213 10.8045 10.3213 14.4942C10.3213 18.184 13.31 21.1727 16.9998 21.1727C20.6896 21.1775 23.6783 18.184 23.6783 14.4942Z"
-              fill="#EC6323" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-            <path
-              d="M26.8804 29.5751C24.1664 31.7207 20.7277 32.9949 17.0048 32.9949C13.2724 32.9949 9.83366 31.7207 7.11963 29.5751C8.10956 25.0328 12.1593 21.6319 17.0048 21.6319C21.836 21.6319 25.8858 25.0328 26.8804 29.5751Z"
-              fill="#EC6323" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-        </a>
+      <div class="sing-in dropdown" @click="showLoginModal = true; toggleBodyOverflow(true)">
+        <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M32.9948 17.0047C32.9948 22.106 30.6076 26.6578 26.8752 29.5802C24.1611 31.7259 20.7224 33 16.9995 33C13.2671 33 9.82841 31.7259 7.11437 29.5802C3.38672 26.6531 0.999512 22.106 0.999512 17.0047C0.999512 8.16163 8.16115 1 17.0042 1C25.8379 1 32.9948 8.16163 32.9948 17.0047Z"
+            fill="#fff" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+          <path
+            d="M23.6783 14.4942C23.6783 10.8045 20.6896 7.8157 16.9998 7.8157C13.31 7.8157 10.3213 10.8045 10.3213 14.4942C10.3213 18.184 13.31 21.1727 16.9998 21.1727C20.6896 21.1775 23.6783 18.184 23.6783 14.4942Z"
+            fill="#EC6323" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+          <path
+            d="M26.8804 29.5751C24.1664 31.7207 20.7277 32.9949 17.0048 32.9949C13.2724 32.9949 9.83366 31.7207 7.11963 29.5751C8.10956 25.0328 12.1593 21.6319 17.0048 21.6319C21.836 21.6319 25.8858 25.0328 26.8804 29.5751Z"
+            fill="#EC6323" stroke="#EC6323" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
       </div>
     </div>
   </nav>
+
+  <div v-if="showLoginModal" class="modal-overlay" @click="hideLoginModal">
+    <div class="modal-content" @click.stop>
+      <LoginForm @close="hideLoginModal" />
+    </div>
+  </div>
 </template>
 
-<script scoped>
-import { defineComponent, ref, onMounted } from "vue";
+<script>
+import { defineComponent, ref, onMounted, onBeforeUnmount } from "vue";
+import LoginForm from "./LoginForm.vue";
 
 export default defineComponent({
   name: "TopNavbar",
+  components: {
+    LoginForm,
+  },
   setup() {
     const headRef = ref(null);
-    onMounted(() => {
-      window.addEventListener("scroll", () => {
-        var curr = window.scrollY;
+    const showLoginModal = ref(false);
+    const showServiceDropdown = ref(false);
+    const showBusinessDropdown = ref(false);
+    const showIconsDropdown = ref(false);
+
+    const handleScroll = () => {
+      if (headRef.value) {
+        const curr = window.scrollY;
         if (curr >= 10) {
           headRef.value.classList.add("fx");
         } else {
           headRef.value.classList.remove("fx");
         }
-      });
+      }
+    };
+
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
     });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+
+    const toggleBodyOverflow = (toggle) => {
+      if (toggle) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
+    };
+
+    const hideLoginModal = () => {
+      showLoginModal.value = false;
+      toggleBodyOverflow(false);
+    };
+
     return {
-      headRef
+      headRef,
+      showLoginModal,
+      showServiceDropdown,
+      showBusinessDropdown,
+      showIconsDropdown,
+      toggleBodyOverflow,
+      hideLoginModal
     };
   },
-  data() {
-    return {
-      showServiceDropdown: false,
-      showBusinessDropdown: false,
-      showIconsDropdown: false
-    };
-  }
 });
 </script>
 
@@ -103,5 +138,29 @@ export default defineComponent({
 .wrapper .fixed-top {
   position: sticky;
   z-index: 2100;
+}
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2200;
+  height: 100vh;
+}
+
+
+.modal-content {
+  background: #a1e8b9;
+  padding: 30px;
+  max-width: 470px;
+  width: 100%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 25px;
 }
 </style>
