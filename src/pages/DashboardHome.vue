a957315e5515c003b26f0c0ee1f7cee9723514ea
OlegBondarUA <Petrovichteh@gmail.com>
2024-06-13 18:32:15 +0300

    upd style for dashboard sidebar

--- src/pages/DashboardHome.vue
+++ src/pages/DashboardHome.vue
@@ -1,110 +1,160 @@
 <template>
-    <div class="wrapper">
-      <div class="sidebar">
-        <router-link to="/" class="logo">Логотип</router-link>
-        <nav class="nav">
-          <router-link to="/dashboard" class="nav-item">Інформаційна панель</router-link>
-          <router-link to="/cars" class="nav-item">Автомобілі</router-link>
-          <router-link to="/drivers" class="nav-item">Водії</router-link>
-          <router-link to="/fleet-management" class="nav-item">Керування автопарком</router-link>
-          <router-link to="/driver-schedule" class="nav-item">Розклад водіїв</router-link>
-          <router-link to="/aggregator-integration" class="nav-item">Підключення агрегаторів</router-link>
-        </nav>
-        <button class="logout-button" @click="logout">Вийти</button>
-      </div>
-      <div class="main-content">
-        <header class="header">
-          <h1>Назва автопарку</h1>
-        </header>
-        <div class="content">
-          <router-view></router-view>
-        </div>
+  <div class="wrapper">
+    <div class="sidebar">
+      <router-link to="/">
+        <img src="/images-cloud/dasboard-home-sidebar/logo.svg" alt="logo">
+      </router-link>
+      <nav class="nav">
+        <router-link to="/dashboard" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/house.svg" alt="house" class="nav-icon">
+          <span>Інформаційна панель</span>
+        </router-link>
+        <router-link to="/cars" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/vehicle.svg" alt="vehicle" class="nav-icon">
+          <span>Автомобілі</span>
+        </router-link>
+        <router-link to="/drivers" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/drivers.svg" alt="drivers" class="nav-icon">
+          <span>Водії</span>
+        </router-link>
+        <router-link to="/fleet-management" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/key.svg" alt="key" class="nav-icon">
+          <span>Керування автопарком</span>
+        </router-link>
+        <router-link to="/driver-schedule" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/calendar.svg" alt="calendar" class="nav-icon">
+          <span>Розклад водіїв</span>
+        </router-link>
+        <router-link to="/aggregator-integration" class="nav-item">
+          <img src="/images-cloud/dasboard-home-sidebar/agregators.svg" alt="agregators" class="nav-icon">
+          <span>Підключення агрегаторів</span>
+        </router-link>
+      </nav>
+      <button class="logout-button" @click="logout">Вийти</button>
+      <button class="collapsing-sidebar"><img src="/images-cloud/dasboard-home-sidebar/arrows.svg"
+          alt="arrows"></button>
+    </div>
+    <div class="main-content">
+      <header class="header">
+        <h1>Назва автопарку</h1>
+      </header>
+      <div class="content">
+        <router-view></router-view>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    methods: {
-      logout() {
-        // Логіка виходу з системи
-        console.log("Вихід з системи");
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // Логіка виходу з системи
+      console.log("Вихід з системи");
     }
   }
-  </script>
-  
-  <style scoped>
-  .wrapper {
-    display: flex;
-    height: 100vh; /* Встановлення висоти на весь екран */
-  }
-  
-  .sidebar {
-    width: 380px; /* Встановлення ширини */
-    background-color: #f0f0f0; /* Колір фону за бажанням */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between; /* Розташування елементів зверху і знизу */
-    padding: 20px;
-  }
-  
-  .logo {
-    font-size: 24px;
-    font-weight: bold;
-    text-decoration: none;
-    color: #333;
-    margin-bottom: 20px;
-  }
-  
-  .nav {
-    flex-grow: 1;
-  }
-  
-  .nav-item {
-    display: block;
-    margin-bottom: 15px;
-    text-decoration: none;
-    color: #333;
-  }
-  
-  .nav-item:hover {
-    text-decoration: underline;
-  }
-  
-  .logout-button {
-    background-color: #d9534f;
-    color: white;
-    border: none;
-    padding: 10px;
-    cursor: pointer;
-    text-align: center;
-  }
-  
-  .logout-button:hover {
-    background-color: #c9302c;
-  }
-  
-  .main-content {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .header {
-    background-color: #f8f9fa; /* Світло-сірий фон */
-    padding: 20px;
-    border-bottom: 1px solid #e7e7e7; /* Сірий бордер знизу */
-  }
-  
-  .header h1 {
-    margin: 0;
-    font-size: 24px;
-  }
-  
-  .content {
-    flex-grow: 1;
-    padding: 20px;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 380px;
+  background-color: #a1e8b9;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 20px;
+  position: relative;
+}
+
+.logo {
+  font-size: 24px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #333;
+  margin-bottom: 20px;
+}
+
+.nav {
+  margin-top: 55px;
+}
+
+.nav-item {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 10px;
+  margin-bottom: 25px;
+  text-decoration: none;
+}
+
+.nav-item img {
+  width: 50px;
+  height: auto;
+}
+
+.nav-item span {
+  font-family: var(--second-family);
+  font-weight: 500;
+  font-size: 25px;
+  color: #141e17;
+}
+
+.logout-button {
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  background: #fff;
+  border-radius: 25px;
+  font-family: var(--font-family);
+  font-weight: 700;
+  font-size: 20px;
+  text-align: center;
+  color: #141e17;
+}
+
+.main-content {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  padding: 20px;
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+}
+
+.header h1 {
+  font-family: var(--second-family);
+  font-weight: 600;
+  font-size: 36px;
+  text-transform: uppercase;
+  color: #141e17;
+}
+
+.content {
+  flex-grow: 1;
+  padding: 20px;
+}
+
+.collapsing-sidebar {
+  position: absolute;
+  border-radius: 18px;
+  width: 90px;
+  height: 37px;
+  background: #a1e8b9;
+  border: none;
+  left: 85%;
+  top: 37%;
+  padding-right: 5px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+</style>
